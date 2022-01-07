5cd6b258f099e6d8cbf6fa66ffb1a4cb8e79fd36
Fazza321 <96450914+Fazza321@users.noreply.github.com>
2022-01-07 16:33:44 +0000

    Update planetClass.py

--- planetClass.py
+++ planetClass.py
@@ -1,10 +1,12 @@
-import pygame
 import math
+
+import pygame
+
 size = (1920, 1080)
 
 
 class Planet:
-  def __init__(self, name, radius, colour, speed, distance, mass, angle, orbitcentrex, orbitcentrey):
+  def __init__(self, name, radius, colour, speed, distance, mass, angle, orbiting_planet, centre_of_screen):
     self.y = None
     self.x = None
     self._name = name
@@ -13,42 +15,54 @@ class Planet:
     self._speed = speed
     self._distance = distance
     self._angle = angle
-    self._centreX = orbitcentrex
-    self._centreY = orbitcentrey
+    self._orbiting_planet = orbiting_planet
+    self._centre_of_screen = centre_of_screen
 
   def render(self, screen):
     self.updatePos()
-    pygame.draw.circle(screen, MercuryColour, [size[0]/2, size[1]/2], self._distance, 1)
+    pygame.draw.circle(screen, MercuryColour, [self.centre_x(), self.centre_y()], self._distance, 1)
     pygame.draw.circle(screen, self._colour, [self.x, self.y], self._radius)
 
   def updatePos(self):
     self._angle += self._speed
     xChange = math.sin(self._angle)*self._distance
     yChange = math.cos(self._angle)*self._distance
-    self.x = xChange + self._centreX
-    self.y = yChange + self._centreY
+    self.x = xChange + self.centre_x()
+    self.y = yChange + self.centre_y()
 
-  def getx(self):
-    return (math.sin(self._angle)*self._distance) + self._centreX
+  def centre_x(self):
+    if self._orbiting_planet is not None:
+      return self._orbiting_planet.x
+    else:
+      return self._centre_of_screen[0]
 
-  def gety(self):
-    return (math.cos(self._angle)*self._distance) + self._centreX
+  def centre_y(self):
+    if self._orbiting_planet is not None:
+      return self._orbiting_planet.y
+    else:
+      return self._centre_of_screen[1]
 
-  def speedUp(*planets):
+  def centre_xa(self):
+      return self.x
+
+  def centre_ya(self):
+    return self.y
+
+  @classmethod
+  def speedUp(cls, planets):
     for planet in planets:
       planet._speed += (0.2*(planet._speed))
 
-  def slowDown(*planets):
+  @classmethod
+  def slowDown(cls, planets):
     for planet in planets:
       planet._speed -= (0.2*planet._speed)
 
-  def stop(*planets):
+  @classmethod
+  def stop(cls, planets):
     for planet in planets:
-      planet._speed -= planet._speed
+      planet._speed = 0
 
-  def go(*planets):
-    for planet in planets:
-      planet._speed += planet._speed
 
 SunColour = (230, 230, 0)
 MercuryColour = (131, 134, 139)
